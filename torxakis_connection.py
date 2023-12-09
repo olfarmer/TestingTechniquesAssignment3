0157fe90f38fc4d1d7d9d2910ea010411166f520
Clemens <mail@clemens.jann.xyz>
2023-12-09 15:41:40 +0100

    implement start and stop

--- torxakis_connection.py
+++ torxakis_connection.py
@@ -1,3 +1,4 @@
+import os
 import socket
 from enum import Enum
 
@@ -8,12 +9,19 @@ class Command(Enum):
 
 
 def handle_command(command, socket):
+
     if command == Command.START.value:
         print("Received START command")
-        socket.send(b'started\n')
+        if os.system('docker start synapse') == 0:
+            socket.send(b'started\n')
+        else:
+            socket.send(b'error\n')
     elif command == Command.STOP.value:
         print("Received STOP command")
-        socket.send(b'stopped\n')
+        if os.system('docker stop synapse') == 0:
+            socket.send(b'stopped\n')
+        else:
+            socket.send(b'error\n')
     else:
         print("Unknown command:", command)
         socket.send(b'error\n')
@@ -24,23 +32,21 @@ def start_server():
     port = 7890
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((host, port))
     server_socket.listen()
 
     print(f"Server listening on {host}:{port}")
 
-    while True:
-        client_socket, client_address = server_socket.accept()
-        print(f"Accepted connection from {client_address}")
-        try:
-            while True:
+    client_socket, client_address = server_socket.accept()
+    print(f"Accepted connection from {client_address}")
+    try:
+        while True:
+            data = client_socket.recv(1024).decode('utf-8')
+            handle_command(data.strip(), client_socket)
 
-                data = client_socket.recv(1024).decode('utf-8')
-                print(data)
-                handle_command(data.strip(), client_socket)
-
-        finally:
-            client_socket.close()
+    finally:
+        client_socket.close()
 
 
 if __name__ == "__main__":
